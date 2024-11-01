474d3e55af067af15152a026d0047b831ac033dd
Kolchin Nikita <125834786+Inconsp1cuous@users.noreply.github.com>
2024-11-01 18:00:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import art
-from collections import defaultdict
-import matplotlib.pyplot as plt
-import analyze_text_symbols
-import plot_symbol_frequencies
-import keyboard
-import load_text
+from utils import load_text
+from analyze import analyze_finger_loads
+from plotter import plot_finger_load_comparison, plot_finger_load_comparison_horizontal
+from config import keylout_dd, keylout_shift
 
-def main():
+def main():    
 
-    # Загрузим текст из файла
-    text = load_text("/home/true_jatu/keyboard_vizov/voina-i-mir.txt")
-
-    # Анализируем текст
-    result = analyze_text_symbols(text, keyboard.keylout_dd)
-
-        # Выводим результаты
-    for symbol, count in result.items():
-        print(f"'{symbol}': {count} нажатий")
-
-    # Построение графика частот символов
-    plot_symbol_frequencies(result)
+    # Загрузка текста из файла
+    text = load_text("voina-i-mir.txt")
+    
+    # Анализ нагрузки на пальцы
+    finger_loads_ycu, finger_loads_vyzov = analyze_finger_loads(text, keylout_dd, keylout_shift)
+    
+    # Построение вертикальной сравнительной гистограммы нагрузок на пальцы
+    plot_finger_load_comparison(finger_loads_ycu, finger_loads_vyzov)
+    
+    # Построение горизонтальной сравнительной гистограммы нагрузок на пальцы
+    plot_finger_load_comparison_horizontal(finger_loads_ycu, finger_loads_vyzov)
 
 if __name__ == "__main__":
     main()
