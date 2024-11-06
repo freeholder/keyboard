060d3eae1c502a9ce057eaac4c44dfe678470d0e
freeholder <126153617+freeholder@users.noreply.github.com>
2024-11-06 18:00:38 +0300

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -5,13 +5,13 @@
 import matplotlib.pyplot as plt
 
 def plot_finger_load_comparison(finger_loads_ycu, finger_loads_vyzov):
-   """
+    """
     Строит вертикальную сравнительную гистограмму нагрузок на каждый палец для раскладок ЙЦУКЕН и ВЫЗОВ.
 
     Параметры:
-    - finger_loads_ycu: словарь с частотой нажатий для каждой буквы в раскладке ЙЦУКЕН. 
+    - finger_loads_ycu: словарь с частотой нажатий для каждой буквы в раскладке ЙЦУКЕН.
                         Ключи - названия пальцев, значения - частота нажатий.
-    - finger_loads_vyzov: словарь с частотой нажатий для каждой буквы в раскладке ВЫЗОВ. 
+    - finger_loads_vyzov: словарь с частотой нажатий для каждой буквы в раскладке ВЫЗОВ.
                           Ключи - названия пальцев, значения - частота нажатий.
 
     Гистограмма строится в виде двух вертикальных столбцов для каждой из раскладок,
@@ -20,15 +20,15 @@ def plot_finger_load_comparison(finger_loads_ycu, finger_loads_vyzov):
     fingers = list(finger_loads_ycu.keys())
     loads_ycu = [finger_loads_ycu[finger] for finger in fingers]
     loads_vyzov = [finger_loads_vyzov[finger] for finger in fingers]
-    
+
     # Построение вертикальной сравнительной гистограммы
     x = range(len(fingers))  # позиции для каждого пальца
     plt.figure(figsize=(10, 6))
     bar_width = 0.4  # ширина каждого столбца
-    
+
     plt.bar(x, loads_ycu, width=bar_width, label="ЙЦУКЕН", color="skyblue", align='center')
     plt.bar([pos + bar_width for pos in x], loads_vyzov, width=bar_width, label="ВЫЗОВ", color="salmon", align='center')
-    
+
     plt.xlabel("Пальцы")
     plt.ylabel("Частота нажатий")
     plt.title("Сравнительная нагрузка на пальцы для раскладок ЙЦУКЕН и ВЫЗОВ (вертикальная)")
@@ -38,13 +38,13 @@ def plot_finger_load_comparison(finger_loads_ycu, finger_loads_vyzov):
     plt.show()
 
 def plot_finger_load_comparison_horizontal(finger_loads_ycu, finger_loads_vyzov):
-     """
+    """
     Строит горизонтальную сравнительную гистограмму нагрузок на каждый палец для раскладок ЙЦУКЕН и ВЫЗОВ.
 
     Параметры:
-    - finger_loads_ycu: словарь с частотой нажатий для каждой буквы в раскладке ЙЦУКЕН. 
+    - finger_loads_ycu: словарь с частотой нажатий для каждой буквы в раскладке ЙЦУКЕН.
                         Ключи - названия пальцев, значения - частота нажатий.
-    - finger_loads_vyzov: словарь с частотой нажатий для каждой буквы в раскладке ВЫЗОВ. 
+    - finger_loads_vyzov: словарь с частотой нажатий для каждой буквы в раскладке ВЫЗОВ.
                           Ключи - названия пальцев, значения - частота нажатий.
 
     Гистограмма строится в виде двух горизонтальных полос для каждой из раскладок,
@@ -53,15 +53,15 @@ def plot_finger_load_comparison_horizontal(finger_loads_ycu, finger_loads_vyzov)
     fingers = list(finger_loads_ycu.keys())
     loads_ycu = [finger_loads_ycu[finger] for finger in fingers]
     loads_vyzov = [finger_loads_vyzov[finger] for finger in fingers]
-    
+
     # Построение горизонтальной сравнительной гистограммы
     y = range(len(fingers))  # позиции для каждого пальца
     plt.figure(figsize=(10, 6))
     bar_height = 0.4  # высота каждого столбца
-    
+
     plt.barh(y, loads_ycu, height=bar_height, label="ЙЦУКЕН", color="skyblue", align='center')
     plt.barh([pos + bar_height for pos in y], loads_vyzov, height=bar_height, label="ВЫЗОВ", color="salmon", align='center')
-    
+
     plt.ylabel("Пальцы")
     plt.xlabel("Частота нажатий")
     plt.title("Сравнительная нагрузка на пальцы для раскладок ЙЦУКЕН и ВЫЗОВ (горизонтальная)")
